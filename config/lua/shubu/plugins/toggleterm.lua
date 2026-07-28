0cedd4348d8705bd7ca03dc2e175eeecde4ea40e
Shubham Yelekar <70461738+Shubham-yelekar@users.noreply.github.com>
2026-07-28 10:41:51 +0530

    tabout, toggle term

---
+++ config/lua/shubu/plugins/toggleterm.lua
@@ -0,0 +1,17 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = function()
+    require("toggleterm").setup({
+      size = 15,
+      open_mapping = [[<c-`>]],
+      hide_numbers = true,
+      shade_terminals = false,
+      direction = "horizontal",
+      start_in_insert = true,
+      insert_mappings = true,
+      persist_size = true,
+      close_on_exit = true,
+    })
+  end,
+}
